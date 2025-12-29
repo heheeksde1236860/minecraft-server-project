f7a1d9eadb56649f12a0a886a6e7c0b5682de88e
heheeksde1236860 <vos.maarten050@gmail.com>
2025-12-29 12:25:46 +0100

    new api file
    this file uses a api to convert the username into a UUID so it can be added to the whitelisted users.

---
+++ utils.py
@@ -0,0 +1,34 @@
+import requests
+from PySide6 import QtCore
+
+
+class UUIDFetcher(QtCore.QObject):
+    finished = QtCore.Signal(str, str, str)  # username, uuid, error
+
+    def __init__(self, username, proxy=None):
+        super().__init__()
+        self.username = username
+        self.proxy = proxy
+
+    def run(self):
+        try:
+            url = f"https://playerdb.co/api/player/minecraft/{self.username}"
+            proxies = None
+            if self.proxy:
+                proxies = {
+                    "http": self.proxy,
+                    "https": self.proxy,
+                }
+            response = requests.get(url, timeout=5, proxies=proxies)
+            response.raise_for_status()
+            data = response.json()
+            
+            if data["code"] == "player.found":
+                uuid = data["data"]["player"]["id"]
+                self.finished.emit(self.username, uuid, "")
+            else:
+                self.finished.emit(self.username, "", "Player not found")
+                
+        except Exception as e:
+            self.finished.emit(self.username, "", str(e))
+
