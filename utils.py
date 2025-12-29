0402c830e7e8e32cae0a1f331229f0637964d548
heheeksde1236860 <vos.maarten050@gmail.com>
2025-12-29 12:35:04 +0100

    removed proxy handling

--- utils.py
+++ utils.py
@@ -5,31 +5,23 @@ from PySide6 import QtCore
 class UUIDFetcher(QtCore.QObject):
     finished = QtCore.Signal(str, str, str)
 
-    def __init__(self, username, proxy=None):
+    def __init__(self, username):
         super().__init__()
         self.username = username
-        self.proxy = proxy
 
     def run(self):
         try:
             url = f"https://playerdb.co/api/player/minecraft/{self.username}"
-            proxies = None
-            if self.proxy:
-                proxies = {
-                    "http": self.proxy,
-                    "https": self.proxy,
-                }
-            response = requests.get(url, timeout=5, proxies=proxies)
+
+            response = requests.get(url, timeout=5)
             response.raise_for_status()
             data = response.json()
-            
-            if data["code"] == "player.found":
+
+            if data.get("code") == "player.found":
                 uuid = data["data"]["player"]["id"]
                 self.finished.emit(self.username, uuid, "")
             else:
                 self.finished.emit(self.username, "", "Player not found")
-                
+
         except Exception as e:
             self.finished.emit(self.username, "", str(e))
-
-
